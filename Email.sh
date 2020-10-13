8d7050a1171d819d542faf10c0265312dd2f7b65
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 01:04:51 +0530

    third mandatory part is done

--- Email.sh
+++ Email.sh
@@ -5,7 +5,7 @@ echo "Validing the mandatory second part"
 ##UserCase2
 read -p "Enter Email address " Email
 
-EmailPattern="^[a-zA-Z0-9]+@[a-zA-Z]+"
+EmailPattern="^[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]{2,4}"
 
 if [[ $Email =~ $EmailPattern ]]
 then
