97c22e28d6844f76ebd6017a0219593d800714cb
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 00:54:04 +0530

    Validing the first mandatory part

--- Email.sh
+++ Email.sh
@@ -2,3 +2,14 @@
 
 echo "welcome to Regex Patterns Email Verification"
 
+##UserCase1
+read -p "Enter Email address " Email
+
+EmailPattern="[a-zA-Z0-9]+"
+
+if [[ $Email =~ $EmailPattern ]]
+then
+                echo "Valid"
+else
+                echo "Invalid"
+fi
