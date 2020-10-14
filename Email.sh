818e495a09debd800c3859d2f065e412b3ab566e
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 12:37:21 +0530

    TLD in the last part

--- Email.sh
+++ Email.sh
@@ -2,7 +2,7 @@
 
 echo "Validing the mandatory second part"
 
-##UserCase2
+##UserCase5
 read -p "Enter Email address " Email
 
 EmailPattern="^[0-9a-zA-Z]+([-+_.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
