27055ec3a43de568ad5b01beb675bceaefe3b769
Tharun6986 <m.tharunsrikanthreddy@gmail.com>
2020-10-14 12:46:27 +0530

    All User cases done

--- Email.sh
+++ Email.sh
@@ -1,8 +1,56 @@
 #!/bin/bash -x
 
-echo "Validing the mandatory second part"
+echo "Welcome to Email Verification using Regex patterns"
+
+##UserCase1
+read -p "Enter Email address " Email
+
+EmailPattern="[a-zA-Z0-9]+"
+
+if [[ $Email =~ $EmailPattern ]]
+then
+                echo "Valid"
+else
+                echo "Invalid"
+fi
 
 ##UserCase2
+read -p "Enter Email address " Email
+
+EmailPattern="^[a-zA-Z0-9]+@[a-zA-Z]+"
+
+if [[ $Email =~ $EmailPattern ]]
+then
+                echo "Valid"
+else
+                echo "Invalid"
+fi
+
+##UserCase3
+read -p "Enter Email address " Email
+
+EmailPattern="^[a-zA-Z0-9]+@[a-zA-Z]+.[a-zA-Z]{2,4}"
+
+if [[ $Email =~ $EmailPattern ]]
+then
+                echo "Valid"
+else
+                echo "Invalid"
+fi
+
+##UserCase4
+read -p "Enter Email address " Email
+
+EmailPattern="^[0-9a-zA-Z]+([-+_.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}"
+
+if [[ $Email =~ $EmailPattern ]]
+then
+                echo "Valid"
+else
+                echo "Invalid"
+fi
+
+##UserCase5
 read -p "Enter Email address " Email
 
 EmailPattern="^[0-9a-zA-Z]+([-+_.][0-9a-zA-Z]+)?@[0-9a-zA-Z]+.[a-zA-Z]{2,4}([.][a-zA-Z]{2})?$"
